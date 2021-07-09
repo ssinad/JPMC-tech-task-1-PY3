41c228d91c667b4eba1280c03e4503eae9e2d2e1
ssinad <27953063+ssinad@users.noreply.github.com>
2021-07-08 23:08:02 -0600

    Update client
    Add unit tests

--- client_test.py
+++ client_test.py
@@ -8,6 +8,10 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      ask = quote['top_ask']['price']
+      bid = quote['top_bid']['price']
+      self.assertEqual(getDataPoint(quote), (quote['stock'], bid, ask, (ask + bid) / 2))
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,10 +19,13 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      ask = quote['top_ask']['price']
+      bid = quote['top_bid']['price']
+      self.assertEqual(getDataPoint(quote), (quote['stock'], bid, ask, (ask + bid) / 2))
 
 
   """ ------------ Add more unit tests ------------ """
-
 
 
 if __name__ == '__main__':
